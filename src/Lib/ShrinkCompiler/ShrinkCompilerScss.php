d39607b40ea6f4a4584e256c414b00f1b99ae546
Trent Richardson <trentdrichardson@gmail.com>
2015-05-24 20:17:52 -0400

    Update path for sass

--- src/Lib/ShrinkCompiler/ShrinkCompilerScss.php
+++ src/Lib/ShrinkCompiler/ShrinkCompilerScss.php
@@ -59,11 +59,9 @@ class ShrinkCompilerScss extends ShrinkBase implements ShrinkCompilerInterface{
 			}
 			else{ // cmd line version
 				$cmd = $this->settings['sass']['path'] .' -t '. $style .' '. $file->path;
-				$env = [ 'PATH'=>'/usr/bin:/usr/local/bin:'. pathinfo($this->settings['sass']['path'], PATHINFO_DIRNAME) ];
+				$env = [ 'PATH'=>pathinfo($this->settings['sass']['path'], PATHINFO_DIRNAME) .':/usr/bin:/usr/local/bin' ];
 				$code = $this->cmd($cmd, null, $env);
 				var_dump([$cmd,$env,$this->_error]);
-				// $cmd = $this->settings['sass']['path'] .' -t '. $style .' '. $file->path;
-				// $code = $this->cmd($cmd, null);
 			}
 		}
 
